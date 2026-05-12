4cdbb2dcd407141074142e7ede631c776fd310f3
Gregory Wainer <greg.wainer@gmail.com>
2026-05-12 13:06:54 -0400

    github workflow for astro

--- utprosat/astro.config.ts
+++ utprosat/astro.config.ts
@@ -1,26 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-
 import partytown from '@astrojs/partytown';
-
 import db from '@astrojs/db';
-
 import markdoc from '@astrojs/markdoc';
-
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
-
-import cloudflare from '@astrojs/cloudflare';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://utprosat.github.io',
   integrations: [react(), partytown(), db(), markdoc(), mdx(), sitemap()],
-  adapter: cloudflare(),
 
   vite: {
     plugins: [tailwindcss()]
